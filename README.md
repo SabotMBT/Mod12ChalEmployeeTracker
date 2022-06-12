a6d575284d0b39ca8ba83bab7b4564bf8ec86231
SabotMBT <104104069+SabotMBT@users.noreply.github.com>
2022-06-11 18:19:32 -0600

    Ver 0.1.3

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 Changelog:
 
+- Ver 0.1.3
+
+  - Updated File Structure
+    Finished building SQL Schema and Seeds.
+
+- Ver 0.1.2
+
+  - Added Node Module dependencies
+
 - Ver 0.1.1
+
   - Created initial file structure
--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,27 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    salary INT NOT NULL,
+    dept_id INT
+    FOREIGN KEY (dept_id) REFERENCES departments(id) ON DELETE SET NULL
+)
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+)
